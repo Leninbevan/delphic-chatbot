c1a6feaebb59db5da3cd7baddff6ee763a5c34ca
Leninbevan <charleslenin431@gmail.com>
2025-03-11 16:00:21 +0530

    added side nav

--- src/auth.ts
+++ src/auth.ts
@@ -1,6 +1,6 @@
 import { betterAuth } from 'better-auth';
 export const auth = betterAuth({
-  baseURL: 'http://localhost:5174/api/login',
+  baseURL: 'http://localhost:5173/api/login',
 });
 
 export async function login(email: string, password: string) {
